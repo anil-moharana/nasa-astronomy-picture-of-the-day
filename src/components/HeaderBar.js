66b21628f4083ff436d6c49600f6f01a4d4868f2
anil-moharana <techstack.anil@gmail.com>
2019-10-05 15:02:54 +0530

    Added a camera icon to the header

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -3,10 +3,11 @@ import React from "react";
 const HeaderBar = props => {
   return (
     <div className="ui red inverted center aligned segment">
-      <h2 className="ui icon header">
+      <h1>
         <i className="us flag"></i>
         {props.text}
-      </h2>
+        <i className="camera retro icon" style={{float:'right'}}></i>
+      </h1>
     </div>
   );
 };
